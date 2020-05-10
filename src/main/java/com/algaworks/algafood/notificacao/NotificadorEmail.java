dc7be3fa8993ee1266140a54cd7d529ea8973a7e
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-05-10 15:19:13 -0300

    Aula 2.14

--- src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,37 +1,18 @@
 package com.algaworks.algafood.notificacao;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.modelo.Cliente;
 
-
+@Component
 public class NotificadorEmail implements Notificador {
-	
-	private boolean caixaAlta;
-	private String servidor;
-	
-	public NotificadorEmail(String servidor) {
-		
-		this.servidor = servidor;
-		
-		System.out.println("Construtor chamado");
-		
-	}
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		
-		if(caixaAlta) {
-			
-			mensagem = mensagem.toUpperCase();
-			
-		}
+		System.out.printf("Notificando %s através do e-mail %s: %s\n",
+				cliente.getNome(), cliente.getEmail(), mensagem);
 		
-		System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n",
-				cliente.getNome(), cliente.getEmail(), servidor, mensagem);
-		
-	}
-
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
 	}
 
 }
--- src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -1,18 +1,15 @@
 package com.algaworks.algafood.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.modelo.Cliente;
 import com.algaworks.algafood.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 	
-	public AtivacaoClienteService(Notificador notificador) {
-		
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService: " + notificador);
-		
-	}
-
+	@Autowired //ESTA ANOTAÇÃO PODE SER USADA NO CONSTRUTOR, NO ATRIBUTO OU NO SET
 	private Notificador notificador;
 	
 	public void ativar(Cliente cliente) {
@@ -22,5 +19,6 @@ public class AtivacaoClienteService {
 		notificador.notificar(cliente, "Cadastro efetuado com sucesso!");
 		
 	}
+
 
 }
