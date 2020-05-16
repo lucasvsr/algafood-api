9faed04e0430dc299c8531a845581ee800dfe646
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-05-16 09:00:41 -0300

    Aula 2.21

--- src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -1,5 +1,9 @@
 package com.algaworks.algafood.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,20 +12,32 @@ import com.algaworks.algafood.notificacao.NivelUrgencia;
 import com.algaworks.algafood.notificacao.Notificador;
 import com.algaworks.algafood.notificacao.TipoDoNotificador;
 
-@Component
+//@Component
 public class AtivacaoClienteService {
-	
+
 	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
-	@Autowired(required = false) //ESTA ANOTAÇÃO PODE SER USADA NO CONSTRUTOR, NO ATRIBUTO OU NO SET
+	@Autowired(required = false) // ESTA ANOTAÇÃO PODE SER USADA NO CONSTRUTOR, NO ATRIBUTO OU NO SET
 	private Notificador notificador;
-	
-	public void ativar(Cliente cliente) {
-		
-		cliente.ativar();
-		
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+
+//	@PostConstruct
+	public void init() {
+
+		System.out.println("INICIANDO");
 
 	}
 
+//	@PreDestroy
+	public void destroy() {
 
+		System.out.println("DESTRUINDO");
+
+	}
+
+	public void ativar(Cliente cliente) {
+
+		cliente.ativar();
+
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+
+	}
 }
