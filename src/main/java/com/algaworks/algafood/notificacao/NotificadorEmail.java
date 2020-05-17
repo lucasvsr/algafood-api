f9b901095217ebfb607b8b55a6a6b857eb0f3ecd
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-05-17 08:42:44 -0300

    Aula 2.27

--- src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,7 +1,6 @@
 package com.algaworks.algafood.notificacao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.modelo.Cliente;
