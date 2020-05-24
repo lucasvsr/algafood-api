f4cd43fad0f9dddcbc3f7cca7988a9895dfa5e51
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-05-24 10:38:58 -0300

    Aula 4.20

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,10 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,9 +38,20 @@ public class CozinhaController {
 	}
 	
 	@GetMapping("/{id}")
-	public Cozinha buscar(@PathVariable Long id) {
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
 		
-		return repository.buscar(id);
+		HttpHeaders headers = new HttpHeaders();
+		Cozinha cozinha = repository.buscar(id);
+		
+		headers.add("Teste", "Testando");
+		
+		if(id == 1) return ResponseEntity.status(HttpStatus.NOT_FOUND)
+										 .headers(headers)
+										 .build();
+		
+		if(id == 2) return ResponseEntity.ok(cozinha);
+		
+		return null;
 		
 	}
 
