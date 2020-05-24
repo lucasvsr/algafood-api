2f9662e3421dfe37bbda0144c693542d91d091c6
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-05-24 09:53:49 -0300

    Aula 4.14

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,13 @@ public class CozinhaController {
 	public List<Cozinha> listar() {
 		
 		return repository.listar();
+		
+	}
+	
+	@GetMapping("/{id}")
+	public Cozinha buscar(@PathVariable Long id) {
+		
+		return repository.buscar(id);
 		
 	}
 
