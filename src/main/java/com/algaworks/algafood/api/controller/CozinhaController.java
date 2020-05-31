4b11cfc08b26110d8e7e551c713043cba0792734
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-05-31 10:22:26 -0300

    Aulas 4.24, 4.25 e 4.26

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -2,13 +2,17 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -56,6 +60,50 @@ public class CozinhaController {
 		
 		return repository.adicionar(cozinha);
 		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long id,
+											 @RequestBody Cozinha atualizada) {
+		
+		Cozinha atual = repository.buscar(id);
+		
+		if(atual != null) {
+			
+			BeanUtils.copyProperties(atualizada, atual, "id"); //Copia os dados do item atualizado para o atual (na base). A partir do terceiro parâmetro, passamos o nome das propriedades que não serão copiadas.
+			
+			return ResponseEntity.ok(repository.adicionar(atual));
+			
+		}
+		
+		return ResponseEntity.notFound().build();
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Cozinha> remover(@PathVariable Long id) {
+		
+		try {
+			
+			Cozinha cozinha = repository.buscar(id);
+			
+			if(cozinha != null) {
+				
+				repository.remover(cozinha);
+				
+				return ResponseEntity.noContent().build();
+				
+			}
+			
+			
+		} catch (DataIntegrityViolationException e) {
+				
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+	
+		}
+		
+		return ResponseEntity.notFound().build();
+				
 	}
 
 }
