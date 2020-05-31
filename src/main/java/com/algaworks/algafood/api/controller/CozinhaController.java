398cd1e92c3e64dbf04cf1dc4f76974748e32dc1
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-05-31 11:14:57 -0300

    Aula 4.29 - Desafio

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
@@ -38,13 +36,6 @@ public class CozinhaController {
 	public List<Cozinha> listar() {
 		
 		return repository.listar();
-		
-	}
-	
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml() {
-		
-		return new CozinhasXmlWrapper(repository.listar());
 		
 	}
 	
@@ -77,7 +68,7 @@ public class CozinhaController {
 			
 			BeanUtils.copyProperties(atualizada, atual, "id"); //Copia os dados do item atualizado para o atual (na base). A partir do terceiro parâmetro, passamos o nome das propriedades que não serão copiadas.
 			
-			return ResponseEntity.ok(repository.adicionar(atual));
+			return ResponseEntity.ok(service.salvar(atual));
 			
 		}
 		
