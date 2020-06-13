9b21788b6e4cbcbbbe8b8bf8d42967330fc8641f
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-06-13 17:45:28 -0300

    Aulas 4.33 e 4.34

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,13 +1,19 @@
 package com.algaworks.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +22,7 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController // Esta anotação é a junção de @Controller e @ResponseBody
 @RequestMapping(value = "/restaurantes")
@@ -60,6 +67,57 @@ public class RestauranteController {
 
 		}
 
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> atualizar(@PathVariable Long id, 
+									   @RequestBody Restaurante atualizado) {
+		
+		Restaurante atual = repository.buscar(id);
+		
+		if(atual != null) {
+			
+			BeanUtils.copyProperties(atualizado, atual, "id"); //Copia os dados do item atualizado para o atual (na base). A partir do terceiro parâmetro, passamos o nome das propriedades que não serão copiadas.
+			
+			return ResponseEntity.ok(service.salvar(atual));
+			
+		}
+		
+		return ResponseEntity.notFound().build();		
+		
+	}
+	
+	@PatchMapping("/{id}")
+	public ResponseEntity<?> alterar(@PathVariable Long id, 
+									 @RequestBody Map<String, Object> campos) {
+		
+		Restaurante atual = repository.buscar(id);
+		
+		if(atual == null)
+			return ResponseEntity.notFound().build();
+		
+		merge(campos, atual);
+		
+		return atualizar(id, atual);
+		
+	}
+
+	private void merge(Map<String, Object> origem, Restaurante destino) {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		Restaurante original = mapper.convertValue(origem, Restaurante.class); //CONVERTE O MAP EM ENTIDADE
+		
+		origem.forEach((nome, valor) -> {
+			
+			Field campo = ReflectionUtils.findField(Restaurante.class, nome); //PEGA O CAMPO DA ENTIDADE, O CAMPO N O VALOR
+			campo.setAccessible(true); // SE O CAMPO ESTIVER PRIVADO, PRECISA SETAR ESSE MÉTODO PARA TRUE
+			
+			Object novoValor = ReflectionUtils.getField(campo, original); //PEGA O CAMPO E ATRIBUI O VALOR CONTIDO NO OBJETO CONVERTIDO PARA ENTIDADE
+			
+			ReflectionUtils.setField(campo, destino, novoValor); //PEGA O CAMPO DO OBJETO DESTINO E ATRIBUI O VALOR CONVERTIDO
+			
+		});
+		
 	}
 
 }
