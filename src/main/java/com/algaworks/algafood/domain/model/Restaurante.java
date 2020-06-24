2e75118d826148f6ff31d8a1832f0b0f2fbd17e5
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-06-24 10:25:36 -0300

    Aula 6.2

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -33,5 +36,11 @@ public class Restaurante {
 	@ManyToOne
 	@JoinColumn(nullable = false)
 	private Cozinha cozinha;
+	
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+			   joinColumns = @JoinColumn(name = "restaurante_id"),
+			   inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamento;
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,6 +10,12 @@ insert into estado (id, nome) values (2, 'Rio Grande do Norte');
 insert into estado (id, nome) values (3, 'Paraíba');
 insert into estado (id, nome) values (4, 'Pernambuco');
 
-insert into cidade (id, nome, estado_id) values (1, 'São Lourenço da Mata', 4)
-insert into cidade (id, nome, estado_id) values (2, 'Campina Grande', 3)
-insert into cidade (id, nome, estado_id) values (3, 'Recife', 4)
+insert into cidade (id, nome, estado_id) values (1, 'São Lourenço da Mata', 4);
+insert into cidade (id, nome, estado_id) values (2, 'Campina Grande', 3);
+insert into cidade (id, nome, estado_id) values (3, 'Recife', 4);
+
+insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
+insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
+insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1,1), (1,2), (1,3), (2,3), (3,2), (3,3);
